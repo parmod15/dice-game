7bfb5ac72b87ec16ebf714d3d3a386004b8bfe1a
parmod <parmodmagotra6391@gmail.com>
2023-09-02 21:49:38 +0530

    Added switching players logic

--- script.js
+++ script.js
@@ -34,3 +34,11 @@ const init = function () {
   player1El.classList.remove('player--active');
 };
 init();
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
